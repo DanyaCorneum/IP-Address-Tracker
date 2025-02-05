a9118742173188aaa309bab060d33d81e815a187
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-02-05 11:45:11 +0700

    basic logic of API requests

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,25 @@
 import "dotenv/config";
+console.log(document);
+const apiKey = process.env.APIKEY;
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector(".search-bar__button");
+
+ipInput.addEventListener("keydown", handleKey);
+btn.addEventListener("click", getData);
+
+function getData(event) {
+  event.preventDefault();
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipInput.value}`
+  )
+    .then((resp) => resp.json())
+    .then(console.log);
+}
+
+function handleKey(event) {
+  if (event.key === "Enter") {
+    getData();
+  }
+}
 
 console.log("App is ready");
